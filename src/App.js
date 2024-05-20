1d817cb575e0c373995f3e8cebed8d15767ba068
SUDHIR KUMAR <sudhirk33835@gmail.com>
2024-05-20 21:42:37 +0530

    feat: Add Meals component and MealsSummary subcomponent

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './components/layout/Header';
+import Meals from "./components/Meals/Meals";
 
 function App() {
   return (
     <div className="App">
       <Header />
       {/* Other components go here */}
+      <Meals />
     </div>
   );
 }
